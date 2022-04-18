7a30e8d1f7bcc253c0f45c3685de386ece4cf7fd
Karim Manaouil <k.manaouil@gmail.com>
2022-04-18 17:46:47 +0100

    net: add generic bridged network creation infrastructure

--- start.sh
+++ start.sh
@@ -4,15 +4,15 @@ switch=br0
 BZIMAGE="/home/karim/knc-kernel/arch/x86/boot/bzImage"
 
 create_bridge(){
-	ip link add $switch type bridge
-	ip link set $switch up
+	ip link add $1 type bridge
+	ip link set $1 up
 }
 
 create_tap(){
 	if [ -n "$1" ];then
 		ip tuntap add $1 mode tap user `whoami`
 		ip link set $1 up
-		ip link set $1 master $switch
+		ip link set $1 master $2
 	else
 		echo "Error: no interface specified"
 		exit 1
@@ -33,29 +33,42 @@ launch_vm(){
 		-m 2G \
 		-kernel $BZIMAGE \
 		-append "root=/dev/sda rw console=ttyS0" \
-		-hda disk$1.img \
+		-hda "$1.img" \
 		-nographic \
-		-netdev tap,id=mynet$1,ifname=tap$1,script=no,downscript=no \
-		-device e1000,netdev=mynet$1,mac=52:54:00:12:34:5$1
+		-netdev tap,id=mynet1,ifname="$2",script=no,downscript=no \
+		-device e1000,netdev=mynet1,mac="54:54:00:00:$(($RANDOM%100)):$(($RANDOM%100))"
 }
 
+# ./vmctl network br tap1 tap2 ...
+# ./vmctl vm name tap
 main(){
-	check_network=$(ip link sh $switch 2>&1)
-	ret=$?
-
+        # Because of tap
 	if [ "$(id -u)" -ne 0 ]; then
 		echo "You must be root!"
 		exit 1
-	fi
+        fi
 
-	if [ "$ret" -eq 1 ]; then
-		create_bridge
-		create_tap tap1
-		create_tap tap2
-		allow_vm_ping
-	fi
+        if [ "$1" == "network" ]; then
+                for iface in "${@:2}"; do
+                        check_network=$(ip link sh "$iface" 2>&1)
+                        if [ "$?" -eq "0" ]; then
+                                echo "Network $iface already exists!"
+                                exit 1
+                        fi
+                done
 
-	launch_vm $1
+                create_bridge "$2"
+                echo "Created bridge $2"
+
+                # Enslave the taps to the bridge
+                for tap in "${@:3}"; do
+                        create_tap $tap "$2"
+                        echo "Created $tap and enslaved to $2"
+                done
+                echo "Done"
+        elif [ "$1" == "vm" ]; then
+                launch_vm "$2" "$3"
+        fi
 }
 
 main $@
